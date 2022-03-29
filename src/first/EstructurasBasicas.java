c17e45c92633dcb8c9ed716ae10fe9ed3ee85f03
Itzel <Itzel.CNE@gmail.com>
2022-03-29 19:55:56 -0300

    GitIgnore

---
+++ src/first/EstructurasBasicas.java
@@ -0,0 +1,17 @@
+package first;
+
+public class EstructurasBasicas {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Salidas por consola");
+		
+		int A = 10;
+		String B = "Por fin tengo Strings!";
+		double C = 1.2;
+		
+		System.out.println(B);
+
+	}
+
+}
