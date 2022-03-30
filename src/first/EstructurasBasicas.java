5c483df0cb40797af3ebe91ff79e9d89b62538d1
Itzel <Itzel.CNE@gmail.com>
2022-03-29 23:38:01 -0300

    Estructuras basicas de Java

--- src/first/EstructurasBasicas.java
+++ src/first/EstructurasBasicas.java
@@ -1,4 +1,5 @@
 package first;
+import java.math.*;
 
 public class EstructurasBasicas {
 
@@ -10,10 +11,48 @@ public class EstructurasBasicas {
 		String B = "Por fin tengo Strings!";
 		double C = 1.2;
 		
-		System.out.println(B);
+		if (A == 0) {
+			System.out.println("Verdadero");
+		}else {
+			System.out.println("Falso");
+		}
 		
-		//as
+		System.out.println("////////////");
+		
+		int array[] = {1, 2, 3, 4, 5};
+		
+		System.out.println(array[1]);
+		
+		//ahora matriz
+		
+		int matriz[][] = {{1, 2, 3} , {4, 5, 6} , {7, 8, 9}};
+		
+		System.out.println("Elemento 0 de el arreglo 1: " + matriz[1][0]);
+		
+		// recorrer arreglo 
+		
+		 int i, j;
+		 
+		
+		for (i = 0; i < 5; i++ ) {
+			
+			System.out.println("Los elementos de el arreglo son: " + array[i]);
+			
+		}
+		
+		//Recorrer una matriz
+		
+		System.out.println("ahora recorre la matriz");
+		
+		for(i = 0; i < matriz.length; i++) {
+			
+			for(j=0; j< matriz.length; j++) {
+				
+				System.out.println("Valores Matriz arreglo(" + (i + 1) + ") pos(" + (j + 1) + "):" + matriz[i][j]);
+				
+			}
 
+		}
 	}
 
 }
