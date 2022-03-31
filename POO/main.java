5c4ff30d350318bb6ac835d7741b4d92e5538021
Itzel <Itzel.CNE@gmail.com>
2022-03-30 23:28:39 -0300

    Introduccion a POO java

--- POO/main.java
+++ POO/main.java
@@ -6,7 +6,7 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		Usuario[] Usuarios = new Usuario [2];
+		Usuario[] Usuarios = new Usuario [2]; // declarado como arreglo, tambien se puede declarar normal usando Usuario (nombre de la instancia) = new Usuario(parametros) (este ultimo es el constructuor de la clase)
 		
 		Usuarios[0] = new Usuario("Itzel", 18, 2004, 1, 21);	
 		Usuarios[1] = new Usuario("UsuarioTest", 20, 2015, 1, 21);	
